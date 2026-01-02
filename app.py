73b5dda972ca4f3134c2ea48266a5d654b82f48f
Aatir <aatirali18@gmail.com>
2026-01-02 11:59:05 +0530

    fix some port and python main file

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def predict():
     return render_template('index.html',result = result)
 
 # python main
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
